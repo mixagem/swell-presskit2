b2812cf6048466780231f1ef321d6608eec0efa7
Filipe Teixeira <filipeteixeira141@gmail.com>
2023-09-04 22:12:46 +0100

    more testing

--- main.js
+++ main.js
@@ -1,104 +1,83 @@
-
-const mixStartFinishConfig = {
-    start: ['section-content animate__animated hidden'],
-    finish: ['section-content animate__animated zoomed-out']
+const funkyElements = {
+    'section#origins>div>p:first-of-type': {
+        // (aka distance from bottom)
+        viewports: [
+            [0, 30],
+            [30, 55],
+            [55, 100]
+        ],
+        classes: [
+            { classesToAdd: ['invisible'], removeAll: true },
+            { classesToAdd: ['fade-in'], removeAll: true },
+            { classesToAdd: ['scale-down'], removeAll: true }
+        ]
+    }
 }
-
-const mixStlyesConfig = [{
-    // .5 até .8
-    offset: .8,
-    matchType: '<=',
-    classesToAdd: ['animate__fadeIn'],
-    classesToRemove: ['hidden', 'zoomed-out']
-}, {
-    // 0 até .5
-    offset: .5,
-    matchType: '<=',
-    classesToAdd: ['zoomed-out'],
-    classesToRemove: []
-}, {
-    // .8 até 1 
-    offset: .8,
-    matchType: '>',
-    classesToAdd: ['hidden'],
-    classesToRemove: ['animate__fadeIn', 'zoomed-out']
-},]
-
-const mixStlyesConfig2 = [{
-    // .5 até .85
-    offset: .85,
-    matchType: '<=',
-    classesToAdd: ['animate__fadeIn'],
-    classesToRemove: ['hidden', 'zoomed-out']
-}, {
-    // 0 até .65
-    offset: .65,
-    matchType: '<=',
-    classesToAdd: ['zoomed-out'],
-    classesToRemove: []
-}, {
-    // .8 até 1 
-    offset: .85,
-    matchType: '>',
-    classesToAdd: ['hidden'],
-    classesToRemove: ['animate__fadeIn', 'zoomed-out']
-},]
 
 onscroll = (event) => {
-    magicElement(document.querySelector('section#first .section-content'), mixStartFinishConfig, mixStlyesConfig);
-    magicElement(document.querySelector('section#second .section-content'), mixStartFinishConfig, mixStlyesConfig);
-    magicElement(document.querySelector('section#third .section-content'), mixStartFinishConfig, mixStlyesConfig2)
-    // const vid = document.querySelector('#waves');
-    // vid.currentTime = (1 - percentFromTop) * vid.duration
-    // vid.currentTime = 0;
-};
-
-function magicElement(element, startFinishConfig, stlyesConfig) {
-    const viewportHeight = window.innerHeight;
-
-    // above 1 is yet to be shown, below 0 means scrolled by already
-    const percentFromTop = (element.getBoundingClientRect().top + element.getBoundingClientRect().height) / viewportHeight
-
-    // ignoring elements aready shown / yet to be shown
-    if (percentFromTop > 1) { element.classList = startFinishConfig.start}
-    if (percentFromTop < 0) { element.classList = startFinishConfig.finish}
-
-    stlyesConfig.forEach(config => {
-        switch (config.matchType) {
-            case '<=':
-                if (percentFromTop <= config.offset) {
-                    config.classesToAdd.forEach(className => {
-                        if (!element.classList.contains(className)) {
-                            element.classList.add(className)
-                        }
-                    });
-
-                    config.classesToRemove.forEach(className => {
-                        if (element.classList.contains(className)) {
-                            element.classList.remove(className)
-                        }
-                    });
-                }
-
-                break;
-
-            case '>':
-                if (percentFromTop > config.offset) {
-                    config.classesToAdd.forEach(className => {
-                        if (!element.classList.contains(className)) {
-                            element.classList.add(className)
-                        }
-                    });
-
-                    config.classesToRemove.forEach(className => {
-                        if (element.classList.contains(className)) {
-                            element.classList.remove(className)
-                        }
-                    });
-                }
-
-                break;
-
-        }
-    })
+    funkMyElements()
 }
+
+
+function getElementVisibilityInfo(element) {
+
+    const clientRect = document.querySelector(element).getBoundingClientRect()
+    // altura elemento
+    const viewPortSize = window.innerHeight
+    // posição elemento na janela
+    const positionOnWindow = clientRect.y
+    // percentagem visivel
+    const distanceFromBottom = positionOnWindow / viewPortSize
+
+
+    if (positionOnWindow > viewPortSize || positionOnWindow < -viewPortSize) {
+        visibilityInfo = { animationRange: false }
+        return visibilityInfo
+    }
+
+    switch (true) {
+        case positionOnWindow === 0:
+            visibilityInfo = { animationRange: true, position: 'center', distanceFromBottom: 100 }
+            break;
+        case positionOnWindow > 0:
+            visibilityInfo = { animationRange: true, position: 'top', distanceFromBottom: (1 - distanceFromBottom) * 100 }
+            break;
+        case positionOnWindow < 0:
+            visibilityInfo = { animationRange: true, position: 'bottom', distanceFromBottom: (1 + distanceFromBottom) * 100 }
+            break;
+    }
+
+    return visibilityInfo
+}
+
+function funkMyElements() {
+    Object.keys(funkyElements).forEach(element => {
+        const elementVisibilityInfo = getElementVisibilityInfo(element)
+
+        // element not in need of funk
+        if (!elementVisibilityInfo.animationRange || elementVisibilityInfo.position === 'bottom') { return }
+
+        const funkyStyles = funkyElements[element];
+        const DOMElement = document.querySelector(element);
+
+        for (i = 0; i < funkyStyles.viewports.length; i++) {
+            if (elementVisibilityInfo.distanceFromBottom >= funkyStyles.viewports[i][0] && elementVisibilityInfo.distanceFromBottom < funkyStyles.viewports[i][1]) {
+
+                if (funkyStyles.classes[i].removeAll) {
+                    DOMElement.classList = '';
+                } else {
+                    funkyStyles.classes[i].classesToRemove.forEach(cssClass => {
+                        DOMElement.classList.remove(cssClass)
+                    });
+                }
+
+                funkyStyles.classes[i].classesToAdd.forEach(cssClass => {
+                    DOMElement.classList.add(cssClass)
+
+                });
+            }
+        }
+    });
+}
+
+funkMyElements();
